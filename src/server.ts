e0c51c86914cf5a814f5382b7aa1cd51cdc317a5
LukaMazione <luka.mazione@gmail.com>
2025-03-02 15:12:53 +0000

    server.ts with lint-staged_test

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,19 @@
-import express from 'express'
-import cors from 'cors'
-import { config } from './config'
+import express from 'express';
+import cors from 'cors';
+import { config } from './config';
 import rateLimit from 'express-rate-limit';
 
-const app = express()
+const app = express();
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-app.use(rateLimit({
-  windowMs: 5 * 1000 * 60,
-  limit: 100
-}))
+app.use(
+  rateLimit({
+    windowMs: 5 * 1000 * 60,
+    limit: 100,
+  }),
+);
 
 // eslint-disable-next-line no-console
 app.listen(Number(config.port), '0.0.0.0', () => {
