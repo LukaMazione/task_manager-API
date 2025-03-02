9ee5dbd86b3343a43e1e37230dfe5498bdd78de3
LukaMazione <luka.mazione@gmail.com>
2025-03-02 14:57:19 +0000

    initial express server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,19 @@
-import express from 'express';
-import cors from 'cors';
-import { config } from './config';
+import express from 'express'
+import cors from 'cors'
+import { config } from './config'
+import rateLimit from 'express-rate-limit';
 
-const app = express();
+const app = express()
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json())
 
-app.get("/test", (req, res) => {
-  res.json({
-    "Beacon": "5 h, zakończono",
-    "Side markers": "3h , wtoku"
-  });
-});
+app.use(rateLimit({
+  windowMs: 5 * 1000 * 60,
+  limit: 100
+}))
 
+// eslint-disable-next-line no-console
 app.listen(Number(config.port), '0.0.0.0', () => {
   console.log(`Server is running on port ${config.port}`);
 });
