81d59c73a2d39795b5f27c25c93a76c4323d5986
LukaMazione <luka.mazione@gmail.com>
2025-04-13 10:29:11 +0100

    appendix for routing

--- src/routes/adminRouter.ts
+++ src/routes/adminRouter.ts
@@ -4,3 +4,4 @@ import { requireRole } from '../middlewares/requireRole';
 export const adminRouter = Router();
 
 adminRouter.use(requireRole('admin'));
+// adminRouter.route('/jobcards').get(...).post(...);
